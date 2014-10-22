d06d29a17116bf664472bc65faa6ce7086e2b3e4
Gabriel Formica <gabrielformica93@gmail.com>
2014-10-22 18:34:23 -0430

    Se hacen algunos cambios

--- backpropagation.m
+++ backpropagation.m
@@ -3,29 +3,31 @@
 
 function [newWih, newWho] = backpropagation(valuesi, target, Wih, Who, rate)
     [valuesh, valueso, deltao] = feedforward(Wih, Who, valuesi, target);
-    [deltai, deltah] = backpropagate(Wih, Who, deltao);
-    [newWih, newWho] = updateweights(Wih, Who, valuesi, valuesh, valueso, deltai, deltah, deltao, rate);
+    [deltai, deltah] = backpropagate(Wih, Who, deltao, valuesh);
+    [newWih, newWho] = updateweights(Wih, Who, valuesi, valuesh, valueso, deltah, deltao, rate);
 endfunction
 
-function [newWih, newWho] = updateweights(Wih, Who, valuesi, valuesh, valueso, deltai, deltah, deltao, rate)
+function [newWih, newWho] = updateweights(Wih, Who, valuesi, valuesh, valueso, deltah, deltao, rate)
     % newWih = Wih + (rate * ((valuesi' * (valuesh .* (1 - valuesh))) * deltah))
-    deltaWih = rate * (valuesi' * (deltah' .* (valuesh .* (1 - valuesh))));
+    deltaWih = rate .* (valuesi' * (deltah' .* (valuesh .* (1 - valuesh))));
     newWih = Wih + deltaWih;
-    deltaWho = rate * (valuesh' * (deltao' .* (valueso .* (1 - valueso))));
+
+    deltaWho = rate .* (valuesh' * (deltao' .* (valueso .* (1 - valueso))));
     newWho = Who + deltaWho;
     % newWih = Wih + rate * (deltah * (valuesi' * (valuesh .* (1 - valuesh))))
     % newWho = Who + rate * (deltao * (valuesh' * (valueso .* (1 - valueso))))
 endfunction
 
-function [deltai, deltah] = backpropagate(Wih, Who, deltao)
-    deltah = Who * deltao;
-    deltai = Wih * deltah;
+function [deltai, deltah] = backpropagate(Wih, Who, deltao, valuesh)
+    deltah = valuesh' .* (1 - valuesh') .* (Who * deltao);
+    deltai = Wih * deltah;    % <---------- ?
 endfunction
 
 function [valuesh, valueso, deltao] = feedforward(Wih, Who, inputs, target)
    valuesh = arrayfun(@sigmoid, inputs * Wih);
    valueso = arrayfun(@sigmoid, valuesh * Who);
-   deltao = (target - valueso)';
+   % deltao = ((target - valueso)' .^ 2) ./ 2;
+   deltao = (valueso) .* (1 - valueso) .* (target - valueso)'
 endfunction
 
 % Activation function
--- main.m
+++ main.m
@@ -1,17 +1,19 @@
 source('backpropagation.m')
 
 function main(training_file)
-    Wih = rand(2,10) - 0.5
-    Who = rand(10,1) - 0.5
-    for j = 1 : 1000
+    Wih = rand(2,8) - 0.5
+    Who = rand(8,1) - 0.5
+    for j = 1 : 2000
        [xs,ys,ts] = textread(training_file, '%f %f %f', 'delimiter', ' ');
+       j
        for i = 1:length(xs)
            target = ts(i);
            if (target < 0)
               target = 0;
            endif
-           [Wih, Who] = backpropagation([xs(i), ys(i)], target, Wih, Who, 0.01);
+           [Wih, Who] = backpropagation([xs(i), ys(i)], target, Wih, Who, 0.1);
        endfor
+       j
     endfor
 
     test('set500.txt', Wih, Who)
