0139e8234833b573473d91acc269d240c8851a4d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-10-22 18:39:20 -0430

    Demostrado que funciona la multiplicación para updateweights

--- backpropagation.m
+++ backpropagation.m
@@ -8,35 +8,8 @@ function [newWih, newWho, deltao] = backpropagation(valuesi, target, Wih, Who, r
 end
 
 function [newWih, newWho] = updateweights(Wih, Who, valuesi, valuesh, valueso, der_valuesh, der_valueso, deltai, deltah, deltao, rate)
-    % deltaWih = valuesi' * (deltah' .* der_valuesh);
-    % deltaWho = valuesh' * (deltao' .* der_valueso);
-
-    Wih
-    Who
-    valuesi
-    valuesh
-    valueso
-    der_valuesh
-    der_valueso
-    deltai
-    deltah
-    deltao
-    rate
-
-    deltaWih(1,1) = deltah(1) .* der_valuesh(1) .* valuesi(1);
-    deltaWih(2,1) = deltah(1) .* der_valuesh(1) .* valuesi(2);
-
-    deltaWih(1,2) = deltah(2) .* der_valuesh(2) .* valuesi(1);
-    deltaWih(2,2) = deltah(2) .* der_valuesh(2) .* valuesi(2);
-
-    deltaWih(1,3) = deltah(3) .* der_valuesh(3) .* valuesi(1);
-    deltaWih(2,3) = deltah(3) .* der_valuesh(3) .* valuesi(2);
-
-    deltaWho(1) = deltao .* der_valueso .* valuesh(1)
-    deltaWho(2) = deltao .* der_valueso .* valuesh(2)
-    deltaWho(3) = deltao .* der_valueso .* valuesh(3)
-
-    error('parada');
+    deltaWih = valuesi' * (deltah' .* der_valuesh);
+    deltaWho = valuesh' * (deltao' .* der_valueso);
 
     newWih   = Wih + (rate .* deltaWih);
     newWho   = Who + (rate .* deltaWho);
--- main.m
+++ main.m
@@ -2,14 +2,17 @@ source('backpropagation.m')
 
 function main(training_file)
     [Wih, Who] = initialize(2,3,1)
-    figure;
 
-    for j = 1 : 100
+    for i = 1 : 100
        [xs,ys,ts] = textread(training_file, '%f %f %f', 'delimiter', ' ');
-       for i = 1:length(xs)
-           [Wih, Who] = backpropagation([xs(i), ys(i)], ts(i), Wih, Who, 0.01);
+       for j = 1:length(xs)
+           [Wih, Who, delta] = backpropagation([xs(j), ys(j)], ts(j), Wih, Who, 0.01);
+           deltas(j + i .* length(xs)) = delta;
        end
     end
+
+    figure;
+    plot(deltas, '+');
 
     % plotting the circle
     radius  = 7;
@@ -18,8 +21,9 @@ function main(training_file)
     t = linspace(0,2*pi,100)';
     circsx = radius .* cos(t) + centerx;
     circsy = radius .* sin(t) + centery;
-    plot(circsx,circsy);
 
+    figure;
+    plot(circsx,circsy);
     hold on;
     test(training_file, Wih, Who);
     hold off;
@@ -28,14 +32,17 @@ end
 function test(testing_file, Wih, Who)
     [xs,ys,ts] = textread(testing_file, '%f %f %f', 'delimiter', ' ');
 
+    outputs = [];
     for i = 1 : length(xs)
         [valuesh, _, output, _, deltao] = feedforward(Wih, Who, [xs(i), ys(i)], ts(i));
+        outputs(i) = output;
         if (output >= 0)
            plot(xs(i), ys(i), 'g+')
         else
            plot(xs(i), ys(i), 'r+')
         end
     end
+    % outputs
 end
 
 function [Wih, Who] =  initialize(input_size, hidden_size, output_size)
